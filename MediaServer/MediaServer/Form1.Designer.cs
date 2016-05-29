0a5883282c87035d3099049c8c544b5eb92f179c
PatrickMassler92 <patrick@massler.com>
2016-05-29 18:13:03 +0200

    rev 1

---
+++ MediaServer/MediaServer/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MediaServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.vlcplayer = new AxAXVLC.AxVLCPlugin2();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.playlist = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcplayer)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // vlcplayer
+            // 
+            this.vlcplayer.Enabled = true;
+            this.vlcplayer.Location = new System.Drawing.Point(820, 330);
+            this.vlcplayer.Name = "vlcplayer";
+            this.vlcplayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("vlcplayer.OcxState")));
+            this.vlcplayer.Size = new System.Drawing.Size(320, 240);
+            this.vlcplayer.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 573);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1152, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // playlist
+            // 
+            this.playlist.FormattingEnabled = true;
+            this.playlist.Location = new System.Drawing.Point(820, 12);
+            this.playlist.Name = "playlist";
+            this.playlist.Size = new System.Drawing.Size(320, 303);
+            this.playlist.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1152, 595);
+            this.Controls.Add(this.playlist);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.vlcplayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.vlcplayer)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 vlcplayer;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox playlist;
+    }
+}
+
---
+++ MediaServer/MediaServer/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MediaServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playlist.DoubleClick += new EventHandler(playlist_MouseDoubleClick);
+            string[] filelist = Directory.GetFiles("media");
+            foreach(string s in filelist)
+            {
+                playlist.Items.Add(s.ToString());
+            }
+        }
+
+        void playlist_MouseDoubleClick(object sender, EventArgs e)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            vlcplayer.playlist.add(playlist.SelectedItem.ToString());
+            vlcplayer.playlist.playItem(0);
+        }
+    }
+}
